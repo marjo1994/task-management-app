b5b8b2835c18f9def76c8612ca95978ddcdd8a4e
marjo1994 <m_rhl@live.com>
2025-03-17 11:24:52 -0500

    Improve PUT Lambda Function

--- task-management-backend/update_task/update_task.py
+++ task-management-backend/update_task/update_task.py
@@ -1,14 +1,14 @@
 import json
-from db_connection import get_db_connection 
+from db_connection import get_db_connection
 
 def lambda_handler(event, context):
     try:
         tasks_collection = get_db_connection()
-      
+
         if 'body' not in event:
             return {
                 'statusCode': 400,
-                'body': json.dumps('Error: No body found in the request')
+                'body': json.dumps({'error': 'No body found in the request'})
             }
 
         try:
@@ -16,38 +16,32 @@ def lambda_handler(event, context):
         except json.JSONDecodeError:
             return {
                 'statusCode': 400,
-                'body': json.dumps('Error: Invalid JSON format in request body')
-            }
-
-        allowed_fields = ['title', 'state', 'desc']
-        invalid_fields = [field for field in task_data if field not in allowed_fields]
-
-        if invalid_fields:
-            return {
-                'statusCode': 400,
-                'body': json.dumps(f'Error: Invalid fields: {", ".join(invalid_fields)}')
+                'body': json.dumps({'error': 'Invalid JSON format in request body'})
             }
 
         task_id = event['pathParameters']['id']
-        updated_task = json.loads(event['body'])
-        
+
         result = tasks_collection.update_one(
             {'id': task_id},
-            {'$set': updated_task}
+            {'$set': task_data} 
         )
-        
+
         if result.modified_count > 0:
+            updated_task = tasks_collection.find_one({'id': task_id}, {'_id': 0})
             return {
                 'statusCode': 200,
-                'body': json.dumps('Task updated successfully')
+                'body': json.dumps({
+                    'message': 'Task updated successfully',
+                    'task': updated_task
+                })
             }
         else:
             return {
                 'statusCode': 404,
-                'body': json.dumps('Task not found')
+                'body': json.dumps({'error': 'Task not found'})
             }
     except Exception as e:
         return {
             'statusCode': 500,
-            'body': json.dumps(f'Error: {str(e)}')
+            'body': json.dumps({'error': str(e)})
         }
